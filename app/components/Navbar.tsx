705dca805ba6d386beed0e05bc6ae3e2f8043593
ktang25 <kayleentang718@gmail.com>
2025-10-26 20:42:22 -0400

    Padding and Size changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-white shadow-md">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-5 sm:px-6 lg:px-8  py-1 sm:py-2 lg:py-4">
         <div className="flex justify-between h-16 items-center">
           {/* Logo */}
           <Link href="/" className="text-xl font-semibold text-gray-700">
